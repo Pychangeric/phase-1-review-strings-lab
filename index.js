c90d8f9198118a106405d514b5d78f36fa282cf1
pychangeric <pychangeric@gmail.com>
2023-03-13 17:44:35 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser='eric mutembei';
+
+const welcomeMessage='welcome to flatbook,${currentUser}!';
+
+const excietedwelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting='welcome, ${currentUser.slice(0,1)}!';
+
+
